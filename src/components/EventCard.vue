53bc2ed961c6ddf13a82a343907326b5f88fe71d
Barrington <201711133@spu.ac.za>
2020-11-12 23:23:24 +0200

    Vue Router Essentials

---
+++ src/components/EventCard.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="event-card">
+        <span>@ {{ event.time }} on {{ event.date }}</span>
+        <h4>{{ event.title }}</h4>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'EventCard',
+        props: {
+            event: {
+                type: Object,
+                required: true,
+            }
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .event-card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        padding: 0 20px;
+        width: 250px;
+        height: 140px;
+        cursor: pointer;
+        border: 1px solid #39495c;
+        margin-bottom: 18px;
+        transition: all 0.10s ease-in;
+
+
+        &:hover {
+            transform: scale(1.01);
+            box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2);
+        }
+
+    }
+</style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="event-list">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <h1>Events Calender</h1>
+    <EventCard 
+        v-for="event in events"
+        :key="event.id"
+        :event="event"
+    />
+  </div>
+</template>
+
+<script>
+    // @ is an alias to /src
+    import EventCard from '@/components/EventCard.vue'
+
+    export default {
+        name: 'EventList',
+        components: {
+            EventCard,
+        },
+        data(){
+            return {
+                events: [
+                    {
+                        id: 123456,
+                        category: 'Animal welfare',
+                        title: 'Cat Adoption Day',
+                        description: 'Find your new feline friend at this event.',
+                        location: 'Meow Town',
+                        date: 'January 28, 2022',
+                        time: '12:00',
+                        petsAllowed: true,
+                        organiser: 'Kat Laydee'
+                    },
+                    {
+                        id: 123457,
+                        category: 'Jazz Festival',
+                        title: 'Cape Town Jazz Festival',
+                        description: 'Join in at this Jazz festival.',
+                        location: 'Cape Town',
+                        date: 'January 29, 2022',
+                        time: '10:00',
+                        petsAllowed: false,
+                        organiser: 'Astro Media'
+                    },
+                ],
+            };  
+        }
+    };
+</script>
+
+<style lang="scss" scoped>
+    .event-list {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
